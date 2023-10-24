f005fdf04ae953143ebd577534cc58c53d07c6ff
negxative <89023129+negxative@users.noreply.github.com>
2023-10-24 14:37:55 +0530

    login file update

--- src/components/login/Login.tsx
+++ src/components/login/Login.tsx
@@ -1,18 +1,23 @@
 import React from "react";
 import { globalContext } from "../../utils/Config";
 import Button from "antd/es/button";
-import {useNavigate} from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 const CLIENT_ID = "74625cc4437a43a0b38c94a2e3ae8385";
-const REDIRECT_URI = "http://127.0.0.1:5173/";
+const REDIRECT_URI = "https://statzify.netlify.app/";
 const AUTH_ENDPOINT = "http://accounts.spotify.com/authorize";
 const RESPONSE_TYPE = "token";
-const scopes = ["user-read-private", "user-read-email", "user-library-read","user-top-read"];
+const scopes = [
+  "user-read-private",
+  "user-read-email",
+  "user-library-read",
+  "user-top-read",
+];
 
 export const Login = () => {
   const { setToken } = React.useContext(globalContext) ?? {
     setToken: () => {},
   };
-  const navigate=useNavigate();
+  const navigate = useNavigate();
   React.useEffect(() => {
     const hash = window.location?.hash;
     let responseToken: string | undefined =
@@ -24,7 +29,7 @@ export const Login = () => {
         .find((elem) => elem.startsWith("access_token"))
         ?.split("=")[1];
       window.location.hash = "";
-      if(responseToken){
+      if (responseToken) {
         localStorage.setItem("token", responseToken);
         setToken(responseToken);
         navigate("/categories/topArtists");
@@ -34,11 +39,21 @@ export const Login = () => {
 
   const onClickHandler = () => {
     window.open(
-      `${AUTH_ENDPOINT}?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=${RESPONSE_TYPE}&scope=${scopes.join('%20')}`
+      `${AUTH_ENDPOINT}?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=${RESPONSE_TYPE}&scope=${scopes.join(
+        "%20"
+      )}`
     );
   };
 
-  return <Button type={"link"} className="text-4xl text-green-500" onClick={onClickHandler}>Login</Button>;
+  return (
+    <Button
+      type={"link"}
+      className="text-4xl text-green-500"
+      onClick={onClickHandler}
+    >
+      Login
+    </Button>
+  );
 };
 
 export default Login;
